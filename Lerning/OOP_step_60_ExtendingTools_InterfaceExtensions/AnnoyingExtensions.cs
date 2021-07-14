95f810a785666ef8140c184ff00325bcb36a0eb2
ARTAK BEGLARYAN <35765041+xArtDTx@users.noreply.github.com>
2021-07-14 16:31:22 +0300

    Interface Extensions

---
+++ Lerning/OOP_step_60_ExtendingTools_InterfaceExtensions/AnnoyingExtensions.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP_step_60_ExtendingTools_InterfaceExtensions
+{
+    static class AnnoyingExtensions
+    {
+        public static void PrintDataAndBeep(this System.Collections.IEnumerable iterrator)
+        {
+            foreach (var item in iterrator)
+            {
+                Console.WriteLine(item);
+                Console.Beep();
+            }
+        }
+    }
+}
---
+++ Lerning/OOP_step_60_ExtendingTools_InterfaceExtensions/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP_step_60_ExtendingTools_InterfaceExtensions
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("===== Fun with Compatible Type =====\n");
+
+            //System Array realizing IEnumerable!!!
+            string[] data = { "Wow", "this", "is", "sort", "annoying", "but", "in", "a", "weired", "way", "fun"};
+
+            data.PrintDataAndBeep();
+            Console.WriteLine();
+
+            //List<T> realizing IEnumerable
+            List<int> myInts = new List<int>() { 10, 20, 30, 40};
+            myInts.PrintDataAndBeep();
+            Console.ReadLine();
+        }
+    }
+}
