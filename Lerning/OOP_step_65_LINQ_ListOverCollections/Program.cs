a081d753dd64d2f527b68ba7b000419b71897d7a
ARTAK BEGLARYAN <35765041+xArtDTx@users.noreply.github.com>
2021-07-18 17:56:16 +0300

    ListOverCollections

---
+++ Lerning/OOP_step_65_LINQ_ListOverCollections/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP_step_65_LINQ_ListOverCollections
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("===== Linq over Generic Collection ====\n");
+
+            List<Car> myCar = new List<Car>()
+            {
+                new Car {PetName = "Henry", Color = "Red", Speed = 100, Make = "BMW"},
+                new Car {PetName = "Daisy", Color = "Tan", Speed = 90, Make = "BMW"},
+                new Car {PetName = "Tommy", Color = "Gray", Speed = 10, Make = "VM"},
+                new Car {PetName = "Elvia", Color = "Green", Speed = 5, Make = "Ferrari"},
+                new Car {PetName = "Hadum", Color = "Yellow", Speed = 80, Make = "Yaguar"}
+            };
+
+            GetFastCars(myCar);
+                        
+            Console.ReadLine();
+        }
+
+        static void GetFastCars(List<Car> myCars)
+        {
+            var fastCars = from c in myCars where c.Speed > 90 && c.Make == "BMW" select c;
+
+            foreach (var item in fastCars)
+            {
+                Console.WriteLine($"{item.PetName} is going to fast");
+            }
+        }
+    }
+}
