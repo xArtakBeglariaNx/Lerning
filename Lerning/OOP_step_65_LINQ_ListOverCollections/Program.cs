c9a9f22dc8721bc2a32930af6873412a63a921b4
ARTAK BEGLARYAN <35765041+xArtDTx@users.noreply.github.com>
2021-07-19 23:05:13 +0300

    ListOverCollections(Add)

--- Lerning/OOP_step_65_LINQ_ListOverCollections/Program.cs
+++ Lerning/OOP_step_65_LINQ_ListOverCollections/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -22,7 +23,12 @@ namespace OOP_step_65_LINQ_ListOverCollections
             };
 
             GetFastCars(myCar);
-                        
+            Console.WriteLine();
+            LINQOverArrayList();
+            Console.WriteLine();
+            OfTypeAsFilter();
+
+
             Console.ReadLine();
         }
 
@@ -40,6 +46,45 @@ namespace OOP_step_65_LINQ_ListOverCollections
             foreach (var item in fastCars2)
             {
                 Console.WriteLine($"{item.PetName} : {item.Make} is going to fast");
+            }
+        }
+
+        static void LINQOverArrayList()
+        {
+            Console.WriteLine("===== LINQ Over ArrayList =====\n");
+
+            ArrayList myCars = new ArrayList()
+            {
+                new Car {PetName = "Henry", Color = "Red", Speed = 100, Make = "BMW"},
+                new Car {PetName = "Daisy", Color = "Tan", Speed = 90, Make = "BMW"},
+                new Car {PetName = "Tommy", Color = "Gray", Speed = 10, Make = "VM"},
+                new Car {PetName = "Elvia", Color = "Green", Speed = 5, Make = "Ferrari"},
+                new Car {PetName = "Hadum", Color = "Yellow", Speed = 80, Make = "Yaguar"}
+            };
+
+            var myCarsEnum = myCars.OfType<Car>();
+
+            var fastCars = from c in myCarsEnum where c.Speed > 55 select c;
+
+            foreach (var item in fastCars)
+            {
+                Console.WriteLine("Item: {0}({1}) have speed over 55 km/h", item.PetName, item.Make);
+            }
+        }
+
+        static void OfTypeAsFilter()
+        {
+            Console.WriteLine("==== OfType As Filter ====");
+
+            ArrayList myStuff = new ArrayList();
+
+            myStuff.AddRange(new object[] { 10, 100, 8, false, new Car(), "string data" });
+
+            var myInts = myStuff.OfType<int>();
+
+            foreach (var item in myInts)
+            {
+                Console.WriteLine($"Int value: {item}");
             }
         }
     }
