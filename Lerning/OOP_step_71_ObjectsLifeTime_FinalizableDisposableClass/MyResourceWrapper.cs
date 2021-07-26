fe9fa3046c7a3c09d09ca14bb9ace2687b47c1b4
ARTAK BEGLARYAN <35765041+xArtDTx@users.noreply.github.com>
2021-07-26 18:46:44 +0300

    FinalizableDisposableClass

---
+++ Lerning/OOP_step_71_ObjectsLifeTime_FinalizableDisposableClass/MyResourceWrapper.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP_step_71_ObjectsLifeTime_FinalizableDisposableClass
+{
+    public class MyResourceWrapper : IDisposable
+    {
+        private bool dispozed = false;
+
+        ~MyResourceWrapper()
+        {
+            Console.Beep();
+            //Использольвать для очистки неуправляемых ресурсов
+            //++Не++ вызывать Dispose на неуправляемых ресурсах
+            CleanUp(false); //false означает что очистку запустил сборщик мусора
+        }
+
+        public void Dispose()
+        {
+            //очистить неуправляемые ресурсы
+            //вызвать Dispose для других неуправляемых объектов,
+            //содержащихся внутри
+            CleanUp(true); //true означает что очистку запустил пользователь
+            //Если пользователь вызвал Dispose то финализация
+            //не нужна, поэтому подавить ее
+            GC.SuppressFinalize(this);
+        }
+
+        private void CleanUp(bool disposing)
+        {
+            //проверить не выполнилось ли уже освобождение
+            if (!this.dispozed)
+            {
+                if (disposing)
+                {
+                    //освободить управляемые ресурсы
+                }
+                //очистить управляемые ресурсы
+                dispozed = true;
+            }
+        }
+    }
+}
---
+++ Lerning/OOP_step_71_ObjectsLifeTime_FinalizableDisposableClass/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP_step_71_ObjectsLifeTime_FinalizableDisposableClass
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Dispose()/Destructor Combo platter");
+
+            MyResourceWrapper rw = new MyResourceWrapper();
+            rw.Dispose(); // не приводит к вызову финализатора
+
+            //не вызывая метод Dispose() это приведет к вызову финализатора
+            MyResourceWrapper rw2 = new MyResourceWrapper();
+
+            Console.ReadLine();
+        }
+    }
+}
