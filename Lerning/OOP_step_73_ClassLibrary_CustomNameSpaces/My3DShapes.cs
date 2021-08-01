1989c24a86bf1c4608b05565a61bbbf6b5d06749
ARTAK BEGLARYAN <35765041+xArtDTx@users.noreply.github.com>
2021-08-01 18:01:29 +0300

    ClassLibrary_CustomNameSpaces

---
+++ Lerning/OOP_step_73_ClassLibrary_CustomNameSpaces/My3DShapes.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP_step_73_ClassLibrary_CustomNameSpaces.My3DShapes
+{
+    public class Circle { }
+    public class Square { }
+    public class Hexagon { }
+}
---
+++ Lerning/OOP_step_73_ClassLibrary_CustomNameSpaces/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using MyShapes;
+using OOP_step_73_ClassLibrary_CustomNameSpaces.My3DShapes;
+using The3DCircle = OOP_step_73_ClassLibrary_CustomNameSpaces.My3DShapes.Circle; //псевдоним
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP_step_73_ClassLibrary_CustomNameSpaces
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyShapes.Hexagon h = new MyShapes.Hexagon();
+            MyShapes.Circle c = new MyShapes.Circle();
+            MyShapes.Square s = new MyShapes.Square();
+            My3DShapes.Hexagon hex = new My3DShapes.Hexagon();
+            The3DCircle c2 = new The3DCircle(); //My3DShapes.Circle с использованием псевдонима
+        }
+    }
+}
