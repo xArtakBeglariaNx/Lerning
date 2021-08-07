3819a17ec730a67196edde258d197f29742ad89b
ARTAK BEGLARYAN <35765041+xArtDTx@users.noreply.github.com>
2021-08-07 08:33:11 +0300

    CarLibrary Update to ver. 2_0

--- Lerning/OOP_step_74_ClassLibrary_CarLibrary/Car.cs
+++ Lerning/OOP_step_74_ClassLibrary_CarLibrary/Car.cs
@@ -3,11 +3,20 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Forms;
 
 namespace OOP_step_74_ClassLibrary_CarLibrary
 {
     public enum EngineState
     { engineAlive, engineDead }
+
+    public enum MusicMedia
+    {
+        musicCD,
+        musicTape,
+        musicRadio,
+        musicMP3
+    }
 
     public abstract class Car
     {
@@ -19,9 +28,12 @@ namespace OOP_step_74_ClassLibrary_CarLibrary
         public EngineState EngineState => engState;
 
         public abstract void TurboBoost();
+        public void TurnOnRadio(bool musicOn, MusicMedia mm) =>
+            MessageBox.Show(musicOn ? "Jamming" : "Quet time");
         public Car() { }
         public Car(string name, int currSpeed, int maxSpeed)
         {
+            MessageBox.Show("Car Library Version 2.0!");
             PetName = name;
             CurrentSpeed = currSpeed;
             MaxSpeed = maxSpeed;
