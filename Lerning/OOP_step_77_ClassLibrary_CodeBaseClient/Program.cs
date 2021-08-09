333320a24dc32d1d075c08ce9fd5b4b79c2acc94
ARTAK BEGLARYAN <35765041+xArtDTx@users.noreply.github.com>
2021-08-09 21:36:52 +0300

    ClassLibrary_CodeBase

---
+++ Lerning/OOP_step_77_ClassLibrary_CodeBaseClient/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using OOP_step_74_ClassLibrary_CarLibrary;
+
+namespace OOP_step_77_ClassLibrary_CodeBaseClient
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("==== Code Base Client ====");
+
+            SportCars sportCars = new SportCars();
+            Console.WriteLine("Sports car has been allocated");
+            Console.ReadLine();
+        }
+    }
+}
