ca4b687acef322204eca155d3c1fbe98e0da276b
ARTAK BEGLARYAN <35765041+xArtDTx@users.noreply.github.com>
2021-08-11 16:56:37 +0300

    ClassLibrary_AppConfig

---
+++ Lerning/OOP_step_78_ClassLibrary_AppConfigReaderApp/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Configuration;
+
+namespace OOP_step_78_ClassLibrary_AppConfigReaderApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            AppSettingsReader ar = new AppSettingsReader();
+            int numberOfTimes = (int)ar.GetValue("RepeatCount", typeof(int));
+            string textColor = (string)ar.GetValue("TextColor", typeof(string));
+
+            Console.ForegroundColor = (ConsoleColor)Enum.Parse(typeof(ConsoleColor), textColor);
+
+            for (int i = 0; i < numberOfTimes; i++)
+            {
+                Console.WriteLine($"N:{i+1} Howdy");
+            }
+            Console.ReadLine();
+        }
+    }
+}
