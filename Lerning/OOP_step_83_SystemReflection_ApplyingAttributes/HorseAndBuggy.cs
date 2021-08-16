c09493e48bcb5dad15efc2b4bfead8b8b55fcb4d
ARTAK BEGLARYAN <35765041+xArtDTx@users.noreply.github.com>
2021-08-16 19:47:23 +0300

    SystemReflection_ApplyingAttributes

---
+++ Lerning/OOP_step_83_SystemReflection_ApplyingAttributes/HorseAndBuggy.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP_step_83_SystemReflection_ApplyingAttributes
+{
+    [Serializable, Obsolete("Use another vehicle")]
+    /*  [Serializable]
+        [Obsolete("Use another vehicle")]
+    we can write attributes to use this example*/
+    public class HorseAndBuggy
+    {
+
+    }
+}
---
+++ Lerning/OOP_step_83_SystemReflection_ApplyingAttributes/Motorcycle.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP_step_83_SystemReflection_ApplyingAttributes
+{
+    [Serializable]
+    public class Motorcycle
+    {
+        /// <summary>
+        /// this field not saved
+        /// </summary>
+        [NonSerialized]
+        float weightOfCurrentPassengers;
+
+        /// <summary>
+        /// this fields also [Serializable]
+        /// </summary>
+        bool hasRadioSystem;
+        bool hasHeadSet;
+        bool hasSyssyBar;
+
+    }
+}
