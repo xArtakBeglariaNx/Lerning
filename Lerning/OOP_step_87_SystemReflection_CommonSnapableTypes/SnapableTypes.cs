07909dbd3eeceddfc1706c4d05ad6cc954edf498
ARTAK BEGLARYAN <35765041+xArtDTx@users.noreply.github.com>
2021-08-17 18:23:33 +0300

    SystemReflection_CommonSnapableTypes

---
+++ Lerning/OOP_step_87_SystemReflection_CommonSnapableTypes/SnapableTypes.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP_step_87_SystemReflection_CommonSnapableTypes
+{
+    public interface IAppFunctionality
+    {
+        void DoIt();
+    }
+
+    [AttributeUsage(AttributeTargets.Class)]
+    public sealed class CompanyinfoAttribute : Attribute
+    {
+        public string CompanyName { get; set; }
+        public string CompanyUrl { get; set; }
+
+    }
+}
