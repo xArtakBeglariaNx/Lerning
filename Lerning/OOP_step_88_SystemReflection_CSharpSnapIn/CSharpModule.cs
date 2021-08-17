9161f577920d90b567a0544e4dc5e5ce128df5a7
ARTAK BEGLARYAN <35765041+xArtDTx@users.noreply.github.com>
2021-08-17 18:40:46 +0300

    SystemReflection_VbSnapIn

---
+++ Lerning/OOP_step_88_SystemReflection_CSharpSnapIn/CSharpModule.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using OOP_step_87_SystemReflection_CommonSnapableTypes;
+
+namespace OOP_step_88_SystemReflection_CSharpSnapIn
+{
+    [Companyinfo(CompanyName = "FooBar", CompanyUrl = "www.foobar.com")]
+    public class CSharpModule : IAppFunctionality
+    {
+        void IAppFunctionality.DoIt()
+        {
+            Console.WriteLine("You have just used the C# snap-in");
+        }
+    }
+}
---
+++ Lerning/OOP_step_89_SystemReflection_VbSnapIn/VbSnapIn.vb
@@ -0,0 +1,10 @@
+﻿Imports OOP_step_87_SystemReflection_CommonSnapableTypes
+
+<Companyinfo(CompanyName:="FooBar", CompanyUrl:="www.foobar.com")>
+Public Class VbSnapIn
+    Implements IAppFunctionality
+
+    Public Sub DoIt() Implements OOP_step_87_SystemReflection_CommonSnapableTypes.IAppFunctionality.DoIt
+        Console.WriteLine("You have just used the VB snap-in")
+    End Sub
+End Class
