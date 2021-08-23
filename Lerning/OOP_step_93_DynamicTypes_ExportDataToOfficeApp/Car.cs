c830795e615d32ed684976c7f56c89b3a3931954
ARTAK BEGLARYAN <35765041+xArtDTx@users.noreply.github.com>
2021-08-23 19:14:20 +0300

    DynamicTypes_ExportDataToOfficeApp

---
+++ Lerning/OOP_step_93_DynamicTypes_ExportDataToOfficeApp/Car.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Office.Interop.Excel;
+
+namespace OOP_step_93_DynamicTypes_ExportDataToOfficeApp
+{
+    public class Car
+    {
+        public string PetName { get; set; }
+        public string Color { get; set; }
+        public string Make { get; set; }
+    }
+}
