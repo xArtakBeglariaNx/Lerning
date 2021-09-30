30da95a5504ffb1c67117739f881aaf06e9d0db6
ARTAK BEGLARYAN <35765041+xArtDTx@users.noreply.github.com>
2021-09-30 16:53:54 +0300

    OOP_step_104_ASync_SimpleMultiThreadApp
    using ThreadState

---
+++ Lerning/OOP_step_104_ASync_SimpleMultiThreadApp/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace OOP_step_104_ASync_SimpleMultiThreadApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("==== Amazing Thread App ====\n");
+            Console.Write("Do you want [1] or [2] threads?");
+            string threadCount = Console.ReadLine();
+
+            Thread primaryThread = Thread.CurrentThread;
+            primaryThread.Name = "Primary";
+            Console.WriteLine($"->{Thread.CurrentThread.Name} is executing Main()");
+
+            //Create working class
+            Printer p = new Printer();
+            switch (threadCount)
+            {
+                case "2":
+                    Thread backgroundThread = new Thread(new ThreadStart(p.PrintNumbers));
+                    backgroundThread.Name = "Secondary";
+                    backgroundThread.Start();
+                    break;
+                case "1":
+                    p.PrintNumbers();
+                    break;
+                default:
+                    Console.WriteLine("I dont know what you want...you get 1 thread");
+                    goto case "1";
+            }
+
+            //doing another additional work
+            MessageBox.Show("Im busy!", "Working in main thread...");
+            Console.ReadLine();
+        }
+    }
+}
