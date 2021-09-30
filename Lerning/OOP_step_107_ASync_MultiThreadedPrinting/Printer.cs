34362fdd6e8aa109f15b8b2aa0bad03b2f002055
ARTAK BEGLARYAN <35765041+xArtDTx@users.noreply.github.com>
2021-09-30 18:54:18 +0300

    OOP_step_107_ASync_MultiThreadedPrinting
    Parallelism troubles

---
+++ Lerning/OOP_step_107_ASync_MultiThreadedPrinting/Printer.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace OOP_step_107_ASync_MultiThreadedPrinting
+{
+    public class Printer
+    {
+        //Show info of thread
+        public void PrintNumbers()
+        {
+            Console.WriteLine($"-> {Thread.CurrentThread.Name} is executing PrintNumbers()");
+
+            //show numbers
+            for (int i = 0; i < 10; i++)
+            {
+                Random r = new Random();
+                Thread.Sleep(1000 * r.Next(0));
+                Console.Write(i + 1);
+            }
+
+            Console.WriteLine();
+        }
+    }
+}
---
+++ Lerning/OOP_step_107_ASync_MultiThreadedPrinting/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using OOP_step_106_ASync_SimpleMultiThreadsChanged;
+
+namespace OOP_step_107_ASync_MultiThreadedPrinting
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("==== Synchronizing Treads ====\n");
+            
+            Thread primaryThread = Thread.CurrentThread;
+            primaryThread.Name = "Primary";
+            Console.WriteLine($"->{Thread.CurrentThread.Name} thread is executing Main()");
+
+            //Create working class and 10 threads which indicate to 1 thread
+            Printer p = new Printer();
+            Thread[] threads = new Thread[10];
+            for (int i = 0; i < 10; i++)
+            {
+                threads[i] = new Thread(new ThreadStart(p.PrintNumbers))
+                {
+                    Name = $"Worker thread # {i}"
+                };
+            }
+
+            //now start all threads
+            foreach (Thread t in threads)
+            {
+                t.Start();
+            }
+            Console.ReadLine();
+        }
+    }
+}
