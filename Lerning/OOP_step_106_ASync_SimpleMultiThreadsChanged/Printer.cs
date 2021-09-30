3fee2a5d7dd372116340945fb312d5a0651524e3
ARTAK BEGLARYAN <35765041+xArtDTx@users.noreply.github.com>
2021-09-30 18:32:39 +0300

    OOP_step_106_ASync_SimpleMultiThreadsChanged
    Add
    +background thread is true

---
+++ Lerning/OOP_step_106_ASync_SimpleMultiThreadsChanged/Printer.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace OOP_step_106_ASync_SimpleMultiThreadsChanged
+{
+    public class Printer
+    {
+        //Show info of thread
+        public void PrintNumbers()
+        {
+            Console.WriteLine($"-> {Thread.CurrentThread.Name} is executing PrintNumbers()");
+
+            //show numbers
+            for (int i = 0; i < 10; i++)
+            {
+                Console.Write(i + 1);
+                Thread.Sleep(2000);
+            }
+
+            Console.WriteLine();
+        }
+    }
+}
