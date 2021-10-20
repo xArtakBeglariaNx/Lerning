b33b56cc9ef2ab5f8719bb5963faa6ab5b8c3fef
ARTAK BEGLARYAN <35765041+xArtDTx@users.noreply.github.com>
2021-10-20 10:01:49 +0300

    OOP_step_118_SysIO_StreamWriterReaderApp

---
+++ Lerning/OOP_step_118_SysIO_StramWriterReaderApp/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.IO;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP_step_118_SysIO_StramWriterReaderApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("--- Fun with StreamWriter / StreamReader ---");
+
+            //get object StreamWriter and write lines data
+            using (StreamWriter writer = File.CreateText(@"C:\Users\artge\source\repos\GitReps\Lerning\OOP_step_118_SysIO_StramWriterReaderApp\remiders.txt"))
+            {
+                writer.WriteLine("Dont forget mothers day in this year");
+                writer.WriteLine("Dont forget fathers day in this year");
+                writer.WriteLine("Dont forget this numbers: ");
+                for (int i = 0; i < 10; i++)
+                {
+                    writer.Write(i + "|");
+                }
+                writer.Write(writer.NewLine);
+            }
+            Console.WriteLine("Created file and wrote some thoughts...\n");
+
+            //Read data from file
+            Console.WriteLine("Here are your thoughts:");
+            using(StreamReader reader = File.OpenText(@"C:\Users\artge\source\repos\GitReps\Lerning\OOP_step_118_SysIO_StramWriterReaderApp\remiders.txt"))
+            {
+                string input = null;
+                while ((input = reader.ReadLine()) != null)
+                {
+                    Console.WriteLine(input);
+                }
+            }
+            Console.ReadLine();
+        }
+    }
+}
