dcdc5c37dabfac888737c77cdedeed0afc32320b
ARTAK BEGLARYAN <35765041+xArtDTx@users.noreply.github.com>
2021-10-20 10:22:47 +0300

    OOP_step_119_SysIO_StringWriterReaderApp

---
+++ Lerning/OOP_step_119_SysIO_StringWriterReaderApp/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP_step_119_SysIO_StringWriterReaderApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("--- Fun with StringWriter / StringReader ---");
+
+            //Create obj StringWriter and write symbols data in memory
+            using (StringWriter stWriter = new StringWriter())
+            {
+                stWriter.WriteLine("Don't forget Mother's day");
+
+                //get copy stWriter (saved in memory) and display on console
+                Console.WriteLine($"Contents of StringWriter: {stWriter}");
+
+                //Get inside object StringBuilder
+                StringBuilder sb = stWriter.GetStringBuilder();
+                sb.Insert(0, "Hey!!");
+                Console.WriteLine($"->{sb}");
+                sb.Remove(0, "Hey!!".Length);
+                Console.WriteLine($"->{sb}");
+
+                //Read data from object StringWriter
+                using (StringReader stReader = new StringReader(stWriter.ToString()))
+                {
+                    string input = null;
+                    while ((input = stReader.ReadLine()) != null)
+                    {
+                        Console.WriteLine(input);
+                    }
+                }
+            }
+            Console.ReadLine();
+        }
+    }
+}
