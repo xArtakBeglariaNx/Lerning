60375fd8b5ec5a98e62542c16f4289a6a7e4f1db
ARTAK BEGLARYAN <35765041+xArtDTx@users.noreply.github.com>
2021-11-03 20:28:39 +0300

    OOP_step_127_ADO.NET_AutoLotDAL

---
+++ Lerning/OOP_step_127_ADO.NET_AutoLotDAL/DataOperations/InventoryDAL.cs
@@ -0,0 +1,222 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using OOP_step_127_ADO.NET_AutoLotDAL.Models;
+
+namespace OOP_step_127_ADO.NET_AutoLotDAL.DataOperations
+{
+    public class InventoryDAL
+    {
+        private readonly string _connectionString;
+        public InventoryDAL() : this(@"Data Source=(localdb)\mssqllocaldb;Integrated Security=true;Initial Catalog=AutoLot")
+        {
+
+        }
+        public InventoryDAL(string connectionString) => _connectionString = connectionString;
+
+        private SqlConnection _sqlConnection = null;
+        private void OpenConnection()
+        {
+            _sqlConnection = new SqlConnection() { ConnectionString = _connectionString };
+            _sqlConnection.Open();
+        }
+
+        private void CloseConnection()
+        {
+            if (_sqlConnection?.State != ConnectionState.Closed)
+            {
+                _sqlConnection?.Close();
+            }
+        }
+
+        public List<Car> GetAllInventory()
+        {
+            OpenConnection();
+
+            //records will be stored here
+            List<Car> inventory = new List<Car>();
+
+            //prepare the command object
+            string sql = "Select From * Inventory";
+            using (SqlCommand command = new SqlCommand(sql, _sqlConnection))
+            {
+                command.CommandType = CommandType.Text;
+                SqlDataReader dataReader = command.ExecuteReader(CommandBehavior.CloseConnection);
+                while (dataReader.Read())
+                {
+                    inventory.Add(new Car
+                    {
+                        CarId = (int)dataReader["CarId"],
+                        Color = (string)dataReader["Color"],
+                        Make = (string)dataReader["Make"],
+                        PetName = (string)dataReader["PetName"]
+                    });
+                }
+                dataReader.Close();
+            }
+            return inventory;
+        }
+
+        public Car GetCar(int id)
+        {
+            OpenConnection();
+            Car car = null;
+            string sql = $"Select From * Inventory where CarId = {id}";
+            using (SqlCommand command = new SqlCommand(sql, _sqlConnection))
+            {
+                command.CommandType = CommandType.Text;
+                SqlDataReader dataReader = command.ExecuteReader(CommandBehavior.CloseConnection);
+                while (dataReader.Read())
+                {
+                    car = new Car
+                    {
+                        CarId = (int)dataReader["CarId"],
+                        Color = (string)dataReader["Color"],
+                        Make = (string)dataReader["Make"],
+                        PetName = (string)dataReader["PetName"]
+                    };
+                }
+                dataReader.Close();
+            }
+            return car;
+        }
+
+        public void InsertAuto(string color, string make, string petName)
+        {
+            OpenConnection();
+            string sql = $"Insert Into Inventory (Make, Color, PetName) Values ({make}, {color}, {petName})";
+            using (SqlCommand command = new SqlCommand(sql, _sqlConnection))
+            {
+                command.CommandType = CommandType.Text;
+                command.ExecuteNonQuery();
+            }
+            CloseConnection();
+        }
+
+        //public void InsertAuto(Car car)
+        //{
+        //    OpenConnection();
+        //    string sql = "Insert Into Inventory (Make, Color, PetName) Values" +
+        //        $"'{car.Make}', '{car.Color}', '{car.PetName}' ";
+        //    using (SqlCommand command = new SqlCommand(sql, _sqlConnection))
+        //    {
+        //        command.CommandType = CommandType.Text;
+        //        command.ExecuteNonQuery();
+        //    }
+        //    CloseConnection();
+        //}
+
+        public void InsertAuto(Car car)
+        {
+            OpenConnection();
+            string sql = "Insert Into Inventory" +
+                "(Make, Color, PetName) Values" + $"(@Make, @Color, @PetName)";
+            using (SqlCommand command = new SqlCommand(sql, _sqlConnection))
+            {
+                SqlParameter parameter = new SqlParameter
+                {
+                    ParameterName = "@Make",
+                    Value = car.Make,
+                    SqlDbType = SqlDbType.Char,
+                    Size = 10
+                };
+                command.Parameters.Add(parameter);
+
+                parameter = new SqlParameter
+                {
+                    ParameterName = "@Color",
+                    Value = car.Color,
+                    SqlDbType = SqlDbType.Char,
+                    Size = 10
+                };
+                command.Parameters.Add(parameter);
+
+                parameter = new SqlParameter
+                {
+                    ParameterName = "@PetName",
+                    Value = car.PetName,
+                    SqlDbType = SqlDbType.Char,
+                    Size = 10
+                };
+                command.Parameters.Add(parameter);
+
+                command.ExecuteNonQuery();
+                CloseConnection();
+            }
+        }
+
+        public void DeleteCar(int id)
+        {
+            OpenConnection();
+            string sql = $"Delete From Inventory where CarId = '{id}'";
+            using (SqlCommand command = new SqlCommand(sql, _sqlConnection))
+            {
+                try
+                {
+                    command.CommandType = CommandType.Text;
+                    command.ExecuteNonQuery();
+                }
+                catch (SqlException ex)
+                {
+                    Exception error = new Exception("That car is on order!", ex);
+                    throw error;
+                }
+                CloseConnection();
+            }
+        }
+
+        public void UpdateCarPetName(int id, string newPetName)
+        {
+            OpenConnection();
+            string sql = $"Update Inventory Set PetName = '{newPetName}' Where CarId = '{id}'";
+            using (SqlCommand command = new SqlCommand(sql, _sqlConnection))
+            {
+                command.ExecuteNonQuery();
+            }
+            CloseConnection();
+        }
+
+        public string LookUpPetName(int carId)
+        {
+            OpenConnection();
+            string carPetName;
+
+            using (SqlCommand command = new SqlCommand("GetPetName", _sqlConnection))
+            {
+                command.CommandType = CommandType.StoredProcedure;
+
+                //input parameters
+                SqlParameter param = new SqlParameter
+                {
+                    ParameterName = "@carId",
+                    SqlDbType = SqlDbType.Int,
+                    Value = carId,
+                    Direction = ParameterDirection.Input
+                };
+                command.Parameters.Add(param);
+
+                //output parametrs
+                param = new SqlParameter
+                {
+                    ParameterName = "@petName",
+                    SqlDbType = SqlDbType.Char,
+                    Size = 10,
+                    Direction = ParameterDirection.Output
+                };
+                command.Parameters.Add(param);
+
+                // Execute the stored proc.
+                command.ExecuteNonQuery();
+
+                //return output param
+                carPetName = (string)command.Parameters["@petName"].Value;
+                CloseConnection();
+            }
+            return carPetName;
+        }
+    }
+}
