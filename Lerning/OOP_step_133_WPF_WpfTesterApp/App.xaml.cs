a6fc0616e1d0d42f98449593973fa8a8b2a9e639
ARTAK BEGLARYAN <35765041+xArtDTx@users.noreply.github.com>
2021-12-10 18:17:35 +0300

    OOP_step_133_WPF_WpfTesterApp

---
+++ Lerning/OOP_step_133_WPF_WpfTesterApp/App.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace OOP_step_133_WPF_WpfTesterApp
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        private void App_OnStartup(object sender, StartupEventArgs e)
+        {
+            Application.Current.Properties["GodMode"] = false;
+
+            foreach (var arg in e.Args)
+            {
+                if (arg.Equals("GodMode", StringComparison.OrdinalIgnoreCase))
+                {
+                    Application.Current.Properties["GodMode"] = true;
+                    break;
+                }
+            }
+        }
+
+        private void App_OnExit(object sender, ExitEventArgs e)
+        {
+
+        }
+    }
+}
---
+++ Lerning/OOP_step_133_WPF_WpfTesterApp/MainWindow.xaml.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace OOP_step_133_WPF_WpfTesterApp
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            this.Closed += MainWindow_OnClosed;
+            this.Closing += MainWindow_Closing;
+        }
+
+        private void MainWindow_Closing(object sender, System.ComponentModel.CancelEventArgs e)
+        {
+            string msg = "Do you want close without saving?";
+            MessageBoxResult result = MessageBox.Show(msg, "My App", MessageBoxButton.YesNo, MessageBoxImage.Warning);
+            if (result == MessageBoxResult.No)
+            {
+                e.Cancel = true;
+            }
+        }
+
+        private void MainWindow_OnClosed(object sender, EventArgs e)
+        {
+            MessageBox.Show("See ya!");
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            if ((bool)Application.Current.Properties["GodMode"])
+            {
+                MessageBox.Show("Cheater!");
+            }
+        }
+
+        private void Window_MouseMove(object sender, MouseEventArgs e)
+        {
+            this.Title = e.GetPosition(this).ToString();
+        }
+
+        private void MyCalendar_SelectedDatesChanged(object sender, SelectionChangedEventArgs e)
+        {
+
+        }
+    }
+}
