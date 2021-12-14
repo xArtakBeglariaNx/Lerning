43fe3e948ebda975d4265de70ce6e90404b34fd8
ARTAK BEGLARYAN <35765041+xArtDTx@users.noreply.github.com>
2021-12-14 20:11:42 +0300

    OOP_step_135_WPF_MyWordPad

---
+++ Lerning/OOP_step_134_WPF_VisualLayoutTester/App.xaml.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace OOP_step_134_WPF_VisualLayoutTester
+{
+    /// <summary>
+    /// Логика взаимодействия для App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+}
---
+++ Lerning/OOP_step_135_WPF_MyWordPad/MainWindow.xaml.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace OOP_step_135_WPF_MyWordPad
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+                
+        private void MouseEnterExitArea(object sender, MouseEventArgs e)
+        {
+
+        }
+
+        private void MouseLeaveArea(object sender, MouseEventArgs e)
+        {
+
+        }
+
+        private void FileExit_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+
+        private void ToolsSpellingHints_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void MouseEnterToolsHintsArea(object sender, MouseEventArgs e)
+        {
+
+        }
+
+    }
+}
