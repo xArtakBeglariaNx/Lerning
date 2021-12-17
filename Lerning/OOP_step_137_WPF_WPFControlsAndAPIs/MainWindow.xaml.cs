7e3f695542593b095b8a5532eb3b8af352164362
ARTAK BEGLARYAN <35765041+xArtDTx@users.noreply.github.com>
2021-12-17 15:24:52 +0300

    OOP_step_137_WPF_WPFControlsAndAPIs
    - Ink API

---
+++ Lerning/OOP_step_137_WPF_WPFControlsAndAPIs/MainWindow.xaml.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.IO;
+using System.Windows.Ink;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace OOP_step_137_WPF_WPFControlsAndAPIs
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            this.MyInkCanvas.EditingMode = InkCanvasEditingMode.Ink;
+            this.inkRadio.IsChecked = true;
+            this.comboColors.SelectedIndex = 0;
+        }
+
+        private void RadioButtonCliked(object sender, RoutedEventArgs e)
+        {
+            switch ((sender as RadioButton)?.Content.ToString())
+            {
+                case "Ink Mode":
+                    this.MyInkCanvas.EditingMode = InkCanvasEditingMode.Ink;
+                    break;
+                case "Erase Mode":
+                    this.MyInkCanvas.EditingMode=InkCanvasEditingMode.EraseByStroke;
+                    break;
+                case "Select Mode":
+                    this.MyInkCanvas.EditingMode = InkCanvasEditingMode.Select;
+                    break;
+            }
+        }
+
+        private void ColorChanged(object sender, SelectionChangedEventArgs e)
+        {
+            string colorToUse = (this.comboColors.SelectedItem as StackPanel)?.Tag.ToString();
+            this.MyInkCanvas.DefaultDrawingAttributes.Color = (Color)ColorConverter.ConvertFromString(colorToUse);
+        }
+
+        private void SaveData(object sender, RoutedEventArgs e)
+        {
+            using (FileStream fs = new FileStream("StrokeData.bin", FileMode.Create))
+            {
+                this.MyInkCanvas.Strokes.Save(fs);
+                fs.Close();
+            }
+        }
+
+        private void LoadData(object sender, RoutedEventArgs e)
+        {
+            using (FileStream fl = new FileStream("StrokeData.bin", FileMode.Open, FileAccess.Read))
+            {
+                StrokeCollection strokes = new StrokeCollection(fl);
+                this.MyInkCanvas.Strokes = strokes;
+            }
+        }
+
+        private void Clear(object sender, RoutedEventArgs e)
+        {
+            this.MyInkCanvas.Strokes.Clear();
+        }
+    }
+}
