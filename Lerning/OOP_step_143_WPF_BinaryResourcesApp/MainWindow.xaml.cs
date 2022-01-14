614752e3e8d0d7ded8b89196ec4a1b5a756e91ab
ARTAK BEGLARYAN <35765041+xArtDTx@users.noreply.github.com>
2022-01-14 21:01:14 +0300

    OOP_step_143_WPF_BinaryResourcesApp

---
+++ Lerning/OOP_step_143_WPF_BinaryResourcesApp/MainWindow.xaml.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace OOP_step_143_WPF_BinaryResourcesApp
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        List<BitmapImage> _images = new List<BitmapImage>();
+        private int _currImage = 0;
+
+        private void MainWindow_OnLoaded(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                //string path = Environment.CurrentDirectory;
+                //MessageBox.Show(path);
+                _images.Add(new BitmapImage(new Uri($@"\Images\ac1.png", UriKind.Relative)));
+                _images.Add(new BitmapImage(new Uri($@"\Images\ac2.png", UriKind.Relative)));
+                _images.Add(new BitmapImage(new Uri($@"\Images\ac3.png", UriKind.Relative)));
+                imageHolder.Source = _images[_currImage];
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void btnPreviouseImage_Click(object sender, RoutedEventArgs e)
+        {
+            if (--_currImage < 0)
+            {
+                _currImage = _images.Count - 1;
+            }
+            imageHolder.Source = _images[_currImage];
+        }
+
+        private void btnNextImage_Click(object sender, RoutedEventArgs e)
+        {
+            if (++_currImage >= _images.Count)
+            {
+                _currImage = 0;
+            }
+            imageHolder.Source = _images[_currImage];
+        }
+
+    }
+}
