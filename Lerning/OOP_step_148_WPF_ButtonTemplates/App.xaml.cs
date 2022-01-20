ea4d9b18ac3199179f186c9d5b1b9d487e9282a1
ARTAK BEGLARYAN <35765041+xArtDTx@users.noreply.github.com>
2022-01-20 20:34:34 +0300

    OOP_step_148_WPF_ButtonTemplates

---
+++ Lerning/OOP_step_148_WPF_ButtonTemplates/App.xaml.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace OOP_step_148_WPF_ButtonTemplates
+{
+    /// <summary>
+    /// Логика взаимодействия для App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+}
