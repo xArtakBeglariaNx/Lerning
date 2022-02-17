b44913267c56fe85b1764dca6d00bb8877e888de
ARTAK BEGLARYAN <35765041+xArtDTx@users.noreply.github.com>
2022-02-17 04:27:13 +0300

    OOP_step_153_WPF_WpfMvvm

---
+++ Lerning/OOP_step_153_WPF_WpfMVVM/OOP_step_153_WPF_WpfMVVM/Cmds/AddCarCommand.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP_step_153_WPF_WpfMvvm.Cmds
+{
+    partial class AddCarCommand : CommandBase
+    {
+        public override bool CanExecute(object parameter)
+        {
+            return parameter != null && parameter is ObservableCollection<Inventory>;
+        }
+
+        public override void Execute(object parameter)
+        {
+            if(parameter is ObservableCollection<Inventory> cars)
+            {
+                var maxCount = cars?.Max(x => x.CarId) ?? 0;
+                cars?.Add(new Inventory { CarId = ++maxCount, Color = "Yellow", Make = "VW", PetName = $"Birdie_v.{maxCount}"});
+            }
+        }
+    }
+}
---
+++ Lerning/OOP_step_153_WPF_WpfMVVM/OOP_step_153_WPF_WpfMVVM/Cmds/ChangeColorCommand.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace OOP_step_153_WPF_WpfMvvm.Cmds
+{
+    public class ChangeColorCommand : CommandBase
+    {        
+        public override bool CanExecute(object parameter)
+        {
+            return (parameter as Inventory) != null;
+        }
+
+        public override void Execute(object parameter)
+        {            
+            ((Inventory)parameter).Color = "Pink";
+        }
+    }
+}
